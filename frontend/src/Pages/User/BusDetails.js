92a67e768b8bc2db2ae1d8ffc2075db9106e8207
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2024-08-19 16:24:48 +0530

    Bus comment Added

--- frontend/src/Pages/User/BusDetails.js
+++ frontend/src/Pages/User/BusDetails.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+
+const BusArray = [
+  {
+    _id: "akjsihd5414jan",
+    busNumber: "AS02145",
+    route: ["collage", "nanakpur", "pinjore", "panchkula", "chandigarh"],
+    capacity: 50,
+    currentLocation: "xyz",
+    status: "running",
+  },
+  {
+    _id: "akjsihtr54ttgyrjan",
+    busNumber: "AS02145",
+    route: ["collage", "nanakpur", "pinjore", "panchkula", "chandigarh"],
+    capacity: 50,
+    currentLocation: "xyz",
+    status: "running",
+  },
+  {
+    _id: "asree5rh2kjsihdjan",
+    busNumber: "AS02145",
+    route: ["collage", "nanakpur", "pinjore", "panchkula", "chandigarh"],
+    capacity: 50,
+    currentLocation: "xyz",
+    status: "running",
+  },
+];
 
 function BusDetails() {
+
+  const {Bus_id} = useParams()
+  console.log(Bus_id)
+
   return (
     <div>
-      
+      <h1>Hello Bus</h1>
     </div>
   )
 }
